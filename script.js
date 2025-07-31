0c9410f95dfbe0892ee2a295348a6c0e87190590
imran-ullash <ullashsoftvence@gmail.com>
2025-07-31 06:03:28 +0600

    hello words added

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 const roll = [1,2,3,4,,5,6,7,8,9,10];
 const roll2 = [1,2,3,4,5,6,7,8,9,10];
-console.log(roll.length === roll2.length ? "Equal length" : "Not equal length");
+const result = (roll.length === roll2.length ? "Equal length" : "Not equal length");
+console.log(result);
+
+// const result2 = user.JOb;
+// console.log(result2 + result);
+
+const user = {
+    name : 'Imran Mahmud Ullash',
+    address: 'Aftabnagar , Dhaka',
+    JOb : "Wordpress developer",
+    class : {
+        forJob : 'bdcalling',
+        forCertificate : {
+            highSchool : 'Kawaljani High School',
+            primary : {
+                year2 : 'Nursery',
+                year3Roll : [12, 12, 43, 45]
+            }
+        }
+    }
+
+};
+
+const result3 = user.class.forCertificate.primary.year3Roll[0];
+console.log(result3);
+
+
